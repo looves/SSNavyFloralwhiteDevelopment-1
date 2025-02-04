ece489ab22ea3fed52810a7ffb97923e93699f94
looves <bryxnjtb@gmail.com>
2025-02-04 00:02:58 -0500

    Update UtilsPacks.js

--- utils/UtilsPacks.js
+++ utils/UtilsPacks.js
@@ -4,7 +4,7 @@ const packs = [
     name: 'Normal Pack', // Name of the pack
     url: 'https://i.imgur.com/VrlTgnB.png', // URL of the pack image
     price: 10, // Price of the pack in coins
-    emoji: '<:npack:1333942830288338974>', // Emoji for the pack
+    emoji: '<:npack:1336199647781458010>', // Emoji for the pack
     cardCount: 3, // Number of cards the pack contains
     rarity: ['1'], // Array of rarities that can appear in this pack
   }, 
@@ -13,7 +13,7 @@ const packs = [
     name: 'Rare Pack', // Name of the pack
     url: 'https://i.imgur.com/inVMJgd.png', // URL of the pack image
     price: 20, // Price of the pack in coins
-    emoji: '<:rpack:1333942854430621728>', // Emoji for the pack
+    emoji: '<:rpack:1336199630806843442>', // Emoji for the pack
     cardCount: 3, // Number of cards the pack contains
     rarity: ['1', '2'], // Array of rarities that can appear in this pack
   },
@@ -22,7 +22,7 @@ const packs = [
     name: 'Ultra Pack', // Name of the pack
     url: 'https://i.imgur.com/m1gWLvf.png', // URL of the pack image
     price: 30, // Price of the pack in coins
-    emoji: '<:upack:1333942875624439909>', // Emoji for the pack
+    emoji: '<:upack:1336199613530509323>', // Emoji for the pack
     cardCount: 3, // Number of cards the pack contains
     rarity: ['2'], // Array of rarities that can appear in this pack
   },
@@ -31,7 +31,7 @@ const packs = [
     name: 'Epic Pack', // Name of the pack
     url: 'https://i.imgur.com/Jgq2tMS.png', // URL of the pack image
     price: 40, // Price of the pack in coins
-    emoji: '<:epack:1333942889012527216>', // Emoji for the pack
+    emoji: '<:epack:1336199665816699002>', // Emoji for the pack
     cardCount: 3, // Number of cards the pack contains
     rarity: ['2', '3'], // Array of rarities that can appear in this pack
   },
